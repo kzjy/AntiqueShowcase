9cd492be7c4dd9fff1ffd8358fe135ccb1684714
kelvin <kelv.zhang@mail.utoronto.ca>
2019-05-20 18:09:27 -0400

    Updates

---
+++ precache-manifest.37dc53795b44706287d37ab0b70afccb.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "326a090c5e5fbe154706f48a866832d7",
+    "url": "/AntiqueShowcase/index.html"
+  },
+  {
+    "revision": "8eabb077a0ba05e00723",
+    "url": "/AntiqueShowcase/static/css/main.76d30ae3.chunk.css"
+  },
+  {
+    "revision": "c14546b9bb1ee609a76c",
+    "url": "/AntiqueShowcase/static/js/2.6a4d10eb.chunk.js"
+  },
+  {
+    "revision": "8eabb077a0ba05e00723",
+    "url": "/AntiqueShowcase/static/js/main.84e756d9.chunk.js"
+  },
+  {
+    "revision": "c7f8c5100b0b59222bb6",
+    "url": "/AntiqueShowcase/static/js/runtime~main.b3981a41.js"
+  },
+  {
+    "revision": "63931b438a7776fbaef9d2abc99e7d39",
+    "url": "/AntiqueShowcase/static/media/avatar.63931b43.png"
+  },
+  {
+    "revision": "fcca7afaffde26b9c8d4c33f1c78139a",
+    "url": "/AntiqueShowcase/static/media/background.fcca7afa.jpg"
+  },
+  {
+    "revision": "825dc4703501b11541915dc598e22b0f",
+    "url": "/AntiqueShowcase/static/media/headerNew.825dc470.PNG"
+  },
+  {
+    "revision": "c7b8994a0bb35cf8a1e34db9fedda8f7",
+    "url": "/AntiqueShowcase/static/media/showcase1.c7b8994a.jpeg"
+  },
+  {
+    "revision": "d652acaae396b1a50090f68c0c49d569",
+    "url": "/AntiqueShowcase/static/media/showcase2.d652acaa.jpeg"
+  },
+  {
+    "revision": "b08a60979b3983715bc227b908755d28",
+    "url": "/AntiqueShowcase/static/media/showcase3.b08a6097.jpeg"
+  },
+  {
+    "revision": "b1944a04ea8b2689c930fbcac62588d5",
+    "url": "/AntiqueShowcase/static/media/showcase4.b1944a04.jpeg"
+  },
+  {
+    "revision": "4afddb78ca7b7d50a894d61f545830b4",
+    "url": "/AntiqueShowcase/static/media/showcase5.4afddb78.jpeg"
+  },
+  {
+    "revision": "d85f21f1e8cbd8987f22e7faa5f94d76",
+    "url": "/AntiqueShowcase/static/media/showcase6.d85f21f1.jpeg"
+  },
+  {
+    "revision": "9a108f409266394bbaf8f968c88930f2",
+    "url": "/AntiqueShowcase/static/media/showcase7.9a108f40.jpeg"
+  },
+  {
+    "revision": "bce639ab42f34198791d03f45ec205ab",
+    "url": "/AntiqueShowcase/static/media/showcase8.bce639ab.jpeg"
+  },
+  {
+    "revision": "909232a0c8507bfd386034395d7eb911",
+    "url": "/AntiqueShowcase/static/media/showcase9.909232a0.jpeg"
+  }
+]);
